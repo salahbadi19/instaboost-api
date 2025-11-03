46703b206e09bdbe6895f7aa4ba113dc4f3c4090
salahbadi19 <salaheedinbadi@gmail.com>
2025-11-03 19:52:16 +0100

    Update models.py

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, String, Boolean, DateTime, Text, ForeignKey
 from sqlalchemy.sql import func
-from .database import Base
+from database import Base  # تعديل هنا
 
 class User(Base):
     __tablename__ = "users"
